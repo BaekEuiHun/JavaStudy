1c9073999657761bdba24b19224360dd2d3514f8
BaekEuiHun <euihunbaek@gmail.com>
2026-01-09 10:53:07 +0900

    불변객체 학습

---
+++ main/java/com/example/java/immutable/AddressMain2.java
@@ -0,0 +1,14 @@
+package com.example.java.immutable;
+
+public class AddressMain2 {
+    public static void main(String[] args) {
+        Address a = new Address("서울");
+        Address b = new Address("서울");
+        System.out.println("b = " + b);
+        System.out.println("a = " + a);
+
+        b.setValue("부산");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
